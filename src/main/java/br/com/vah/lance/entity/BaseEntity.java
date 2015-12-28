2ff92ad16c52d8dc90418b10912b430d9a25f1ad
Jairo A. Portela <Jairoportela@vitoriaapart.com.br>
2015-12-28 18:34:29 -0200

    More adjustments

--- src/main/java/br/com/vah/lance/entity/BaseEntity.java
+++ src/main/java/br/com/vah/lance/entity/BaseEntity.java
@@ -18,13 +18,13 @@ public abstract class BaseEntity implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
